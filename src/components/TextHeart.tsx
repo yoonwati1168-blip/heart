8d927ceaec41cddbb29986469920f13acf240183
yoonwati1168-blip <yoonwati1168@gmail.com>
2026-05-27 12:51:28 +0630

    Update TextHeart.khit

--- src/components/TextHeart.tsx
+++ src/components/TextHeart.tsx
@@ -19,7 +19,7 @@ export default function TextHeart() {
 
     let animationFrameId: number;
     let points: Point[] = [];
-    const text = "i love you";
+    const text = "khit";
     const fontSize = 14;
 
     const resize = () => {
